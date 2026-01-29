5f9623be8f4f55664c4888ee53493957389318f0
Adil Raees <adilraees911@gmail.com>
2026-01-30 00:04:02 +0500

    fixed

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -17,7 +17,7 @@ const Contact: React.FC = () => {
     setIsSubmitting(true);
     
     try {
-      const response = await fetch('http://localhost:5000/api/contact', {
+      const response = await fetch('my-portfolio-website.railway.internal/api/contact', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
